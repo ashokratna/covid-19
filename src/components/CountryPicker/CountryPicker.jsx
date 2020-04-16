62f46053ebd6a589cb57c0289aaa3500db2abbad
Ankur Pandey <ap622098@gmail.com>
2020-04-16 13:17:30 +0530

    Covid tracker first deploy on git

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import { NativeSelect, FormControl } from '@material-ui/core'
+import styles from './CountryPicker.module.css';
+
+import { countries } from '../../api'
+
+const CountryPicker = ({handleCountryChange}) => {
+
+    const [fetchCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchCountries = async () => {
+            setFetchedCountries(await countries());
+        }
+        
+        
+        fetchCountries();
+    },[setFetchedCountries]);
+    return (
+        
+            <FormControl className={styles.formControl}>
+                <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                    <option value="">Global</option>
+                    {fetchCountries.map((country,i) => <option key={i} value={country}>{country}</option>)}
+                </NativeSelect>
+            </FormControl>
+        
+    )
+}
+
+export default CountryPicker;
