a87924f272965cd2449007eaeeb1f680f71f708c
Ankur Pandey <ap622098@gmail.com>
2020-04-16 13:15:58 +0530

    Updates

---
+++ precache-manifest.fc4edc067d367a5b86332086b78bd607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d7507fc9bda6f3ef141e4c33d300c6",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "e472be759e9797492a91",
+    "url": "/covid-19/static/css/main.b9302d69.chunk.css"
+  },
+  {
+    "revision": "95f94c58591b1fb34286",
+    "url": "/covid-19/static/js/2.215421f2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.215421f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e472be759e9797492a91",
+    "url": "/covid-19/static/js/main.eb4e3929.chunk.js"
+  },
+  {
+    "revision": "62596194c0150abcd24f",
+    "url": "/covid-19/static/js/runtime-main.7a4c1f9b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/covid.d7265326.png"
+  }
+]);
